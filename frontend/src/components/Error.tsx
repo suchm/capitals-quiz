340cf683e44969f8060c058f44e5dd5e8bb373dd
Mike Such <mikesuchyo@yahoo.com>
2025-02-24 23:31:49 +0000

    updated error styling

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -1,7 +1,7 @@
 function Error() {
     return (
         <p className="text-center bg-gray-100 p-8 rounded-3xl text-4xl mt-50 font-semibold">
-            There was an error fetching questions.
+            There was an error fetching questions!
         </p>
     );
 }
